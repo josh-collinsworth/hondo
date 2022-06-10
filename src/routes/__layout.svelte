5fbd4d6e1d06a9b0c63abc003de8d9951c121ffd
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2022-06-09 22:30:54 -0500

    First pass through PWABuilder

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -13,13 +13,20 @@
 </script>
 
 
-
 <script lang="ts">
   import '$lib/scss/global.scss'
   import { fade } from 'svelte/transition'
 
   export let path: string
 </script>
+
+
+<svelte:head>
+  <title>Hondo</title>
+  <meta name="description" content="Hondo - guess 100 words">
+  <meta name="author" content="Josh Collinsworth">
+  <link rel="manifest" href="manifest.json" />
+</svelte:head>
 
 
 <main>
