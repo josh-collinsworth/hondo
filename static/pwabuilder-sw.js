640ac0c43857591ef54a162fe57d479ed4603048
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2026-02-18 11:06:20 -0600

    PWA Builder updates

--- static/pwabuilder-sw.js
+++ static/pwabuilder-sw.js
@@ -1,11 +1,11 @@
-// This is the "Offline copy of assets" service worker
+// This is the "Offline copy of pages" service worker
 
-const CACHE = 'pwabuilder-offline';
+const CACHE = "pwabuilder-offline";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener('message', (event) => {
-	if (event.data && event.data.type === 'SKIP_WAITING') {
+self.addEventListener("message", (event) => {
+	if (event.data && event.data.type === "SKIP_WAITING") {
 		self.skipWaiting();
 	}
 });
