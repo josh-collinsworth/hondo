3ed66a577c2d3550acb1fe726ee5470131e01ae2
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2026-07-16 10:12:12 -0500

    Add ability to abandon game in progress

---
+++ src/lib/components/icon/blocks/ExclamationPoint.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let { iconColor = 'currentColor' }: { iconColor?: string } = $props();
+</script>
+
+<svg
+	width="100%"
+	height="100%"
+	viewBox="0 0 30 30"
+	version="1.1"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	xml:space="preserve"
+	xmlns:serif="http://www.serif.com/"
+	style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"
+	><path
+		d="M11.655,0l0,6.311l1.052,12.833l4.628,0l1.052,-12.833l0,-6.311l-6.732,0Zm3.366,30c2.314,0 4.081,-1.767 4.081,-3.997c0,-2.23 -1.767,-3.997 -4.081,-3.997c-2.356,0 -4.123,1.767 -4.123,3.997c0,2.23 1.767,3.997 4.123,3.997Z"
+		style="fill-rule:nonzero;"
+	/></svg
+>
